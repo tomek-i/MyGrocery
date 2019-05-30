a83381acfba9d1320704437a7f01671f5e4d593d
Thomas Iwainski <tomek.iwainski@gmail.com>
2019-05-30 16:11:25 +1000

    re-factored admin based on new migrations

--- src/grocerylist/admin.py
+++ src/grocerylist/admin.py
@@ -1,21 +1,49 @@
 from django.contrib import admin
-from .models import GroceryList,  Item, StoreProducts, Product, Store
+from .models import ShoppingList,  Item, Product, Supermarket
 
 # Register your models here.
 
+"""
 
-@admin.register(GroceryList)
-class GroceryListAdmin(admin.ModelAdmin):
-    pass
+
+class ItemsOnShoppinglist(admin.TabularInline):
+    model = ShoppingBasket
+    extra = 1
+
+
+class ProductsInSupermarket(admin.TabularInline):
+    model = StoreProduct
+    extra = 1
+"""
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_completed_icon', 'item_count',
+                    'item_count_completed', 'item_count_missing')
+    #inlines = [ItemsOnShoppinglist]
+
+    def item_count(self, obj, *args, **kwargs):
+        return obj.items.count()
+    item_count.short_description = 'total items'
+
+    def item_count_completed(self, obj, *args, **kwargs):
+        return obj.items.filter(cart__completed=True).count()
+    item_count_completed.short_description = 'completed'
+
+    def item_count_missing(self, obj, *args, **kwargs):
+        return obj.items.filter(cart__completed=False).count()
+    item_count_missing.short_description = 'missing'
+
+    def is_completed_icon(self, obj):
+        return obj.is_completed()
+    is_completed_icon.boolean = True
+    is_completed_icon.short_description = 'completed ?'
+    is_completed_icon.admin_order_field = 'is_completed'
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(StoreProducts)
-class StoreProductsAdmin(admin.ModelAdmin):
     pass
 
 
@@ -24,6 +52,10 @@ class ProductAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Store)
+@admin.register(Supermarket)
 class StoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address')
+    list_display_links = ('id', 'name')
+    ordering = ['id', ]
+    #inlines = [ProductsInSupermarket]
     pass
